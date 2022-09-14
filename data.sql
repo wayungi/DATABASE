05c6a224f3abb4328d17f6969eee72d0b74665e9
wayungi <fwayungi@gmail.com>
2022-09-14 19:53:27 +0300

    Add records into animals table

--- data.sql
+++ data.sql
@@ -8,3 +8,11 @@ INSERT INTO ANIMALS (NAME, DATE_OF_BIRTH, ESCAPE_ATTEMPTS, NEUTERED, WEIGHT) VAL
 ('Devimon', '2017-05-12', 5, TRUE, 11.00);
 
 
+INSERT INTO ANIMALS (name, date_of_birth, escape_attempts, neutered, weight, spicies)
+VALUES ('Charmander', '2020-02-08',0, TRUE, 11.00, NULL),  
+(' Plantmon', '2021-11-05',2, TRUE, 5.70, NULL),
+('Squirtle', '1993-04-02',3, FALSE, 12.13, NULL),
+('Angemon', '2005-06-07',1, TRUE, 45.00, NULL),
+('Boarmon', '2005-06-07',7, TRUE, 20.40, NULL),
+('Blossom', '1998-10-13',3, TRUE, 17.00, NULL),
+('Ditto', '2022-05-14',4, TRUE, 22.00, NULL);
